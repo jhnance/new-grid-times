678d0beb5ca63927f472294e03ed525762999dd4
Joshua Nance <joshua.h.nance@gmail.com>
2021-09-05 20:02:03 -0500

    Add styles for exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { COLORS, QUERIES } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -36,7 +37,9 @@ const SpecialtyStoryGrid = () => {
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+            <MiniStoryWrapper key={data.id}>
+              <MiniStory {...data} />
+            </MiniStoryWrapper>
           ))}
         </SportsStories>
       </SportsSection>
@@ -46,15 +49,54 @@ const SpecialtyStoryGrid = () => {
 
 const Wrapper = styled.div`
   display: grid;
+  grid-template-areas:
+    'market'
+    'sports';
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas: 'market sports';
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  grid-area: market;
 
-const MarketCards = styled.div``;
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid ${COLORS.gray['300']};
+    padding-right: 1rem;
+  }
+`;
 
-const SportsSection = styled.section``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  gap: 1rem;
+`;
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+  grid-area: sports;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-left: 1rem;
+  }
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+  gap: 1rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    overflow-x: scroll;
+  }
+`;
+
+const MiniStoryWrapper = styled.div`
+  flex: 1 0 220px;
+`;
 
 export default SpecialtyStoryGrid;
